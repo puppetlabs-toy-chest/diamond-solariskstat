e64511d1b2306bfc88143e769fe29b698d70e185
Cody Herriges <cody@herriges.org>
2015-01-06 13:43:20 -0800

    Add LICENSE and README.md, fixup setup.py.
      Commit will add a simple README.md and a LICENSE file.  Also will fix up
      a few issues with setup.py.

---
+++ README.md
@@ -0,0 +1,9 @@
+diamond-solariskstat
+====================
+
+Simple set of [diamond](https://github.com/python-diamond) collectors that use Solaris kstats.
+
+* SolarisFSCollector
+  * Collects virtual filesystem statistics for any mount in /etc/mnttab that produces metrics.
+* SolarisARCCollector
+  * Collects all ARC statistics for the system.
--- setup.py
+++ setup.py
@@ -4,14 +4,36 @@ import os
 from glob import glob
 import platform
 
+data_files = []
+
 if os.name == 'nt':
     pgm_files = os.environ["ProgramFiles"]
     base_files = os.path.join(pgm_files, 'diamond')
 
+def pkgPath(root, path, rpath="/"):
+    """
+        Package up a path recursively
+    """
+    global data_files
+    if not os.path.exists(path):
+        return
+    files = []
+    for spath in os.listdir(path):
+        # Ignore test directories
+        if spath == 'test':
+            continue
+        subpath = os.path.join(path, spath)
+        spath = os.path.join(rpath, spath)
+        if os.path.isfile(subpath):
+            files.append(subpath)
+        if os.path.isdir(subpath):
+            pkgPath(root, subpath, spath)
+    data_files.append((root + rpath, files))
+
 if os.name == 'nt':
-    pkgPath(os.path.join(base_files, 'collectors'), 'src/collectors', '\\')
+    pkgPath(os.path.join(base_files, 'collectors/solariskstat'), 'src/collectors/solariskstat', '\\')
 else:
-    pkgPath('share/diamond/collectors', 'src/collectors')
+    pkgPath('share/diamond/collectors/solariskstat', 'src/collectors/solariskstat')
 
 setup(
     name = "diamond-solariskstat",
@@ -19,9 +41,11 @@ setup(
     package_dir = {'': 'src'},
     packages = find_packages(),
     author = "Puppet Labs",
+    author_email = "info@puppetlabs.com",
     description = "Collection of diamond collectors that extract data from Solaris kstats.",
     license = "ALv2",
     keywords = "diamond solaris kstats kstat zfs",
     url = "http://github.com/puppetlabs/diamond-solariskstat",
-    install_requires = ['diamond'],
+    data_files=data_files,
+    install_requires = ['diamond']
 )
