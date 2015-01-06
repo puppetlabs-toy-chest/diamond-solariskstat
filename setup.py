055802fa42f0992f090b7c784b75e4da331bde98
Cody Herriges <cody@herriges.org>
2015-01-06 15:56:02 -0800

    Make compatible with internal pypi repo
      Commit adds a MANIFEST.in file and a couple fixes to setup.py so that
      this "package-less" package bundles up data files correctly so the
      sdist method of packaging and distribution.

--- setup.py
+++ setup.py
@@ -10,35 +10,16 @@ if os.name == 'nt':
     pgm_files = os.environ["ProgramFiles"]
     base_files = os.path.join(pgm_files, 'diamond')
 
-def pkgPath(root, path, rpath="/"):
-    """
-        Package up a path recursively
-    """
-    global data_files
-    if not os.path.exists(path):
-        return
-    files = []
-    for spath in os.listdir(path):
-        # Ignore test directories
-        if spath == 'test':
-            continue
-        subpath = os.path.join(path, spath)
-        spath = os.path.join(rpath, spath)
-        if os.path.isfile(subpath):
-            files.append(subpath)
-        if os.path.isdir(subpath):
-            pkgPath(root, subpath, spath)
-    data_files.append((root + rpath, files))
-
 if os.name == 'nt':
-    pkgPath(os.path.join(base_files, 'collectors/solariskstat'), 'src/collectors/solariskstat', '\\')
+    data_files.append((os.path.join(base_files, 'collectors/solariskstat'), ['src/collectors/solariskstat/solarisfs.py', 'src/collectors/solariskstat/solarisarc.py']))
 else:
-    pkgPath('share/diamond/collectors/solariskstat', 'src/collectors/solariskstat')
+    data_files.append(('share/diamond/collectors/solariskstat', ['src/collectors/solariskstat/solarisfs.py', 'src/collectors/solariskstat/solarisarc.py']))
 
 setup(
     name = "diamond-solariskstat",
     version = "0.1",
     package_dir = {'': 'src'},
+    data_files = data_files,
     packages = find_packages(),
     author = "Puppet Labs",
     author_email = "info@puppetlabs.com",
@@ -46,6 +27,6 @@ setup(
     license = "ALv2",
     keywords = "diamond solaris kstats kstat zfs",
     url = "http://github.com/puppetlabs/diamond-solariskstat",
-    data_files=data_files,
-    install_requires = ['diamond']
+    install_requires = ['diamond'],
+    zip_safe = 0
 )
